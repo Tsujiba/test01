9b76ddc15afc55bf0808f79b4fb77577f1dae216
辻林侑 <tsujibayashiyu@tsujihayashiyuunoMacBook-Air.local>
2020-01-09 22:56:38 +0900

    OK

--- sample.py
+++ sample.py
@@ -1,3 +1,38 @@
 #sample01
 
-print("Hello world")
+class stack:
+
+    def __init__(self):
+        self.list = []
+    
+    def size(self):
+        return len(self.list)
+    
+    def pop(self):
+        return self.list.pop()
+    
+    def push(self, value):
+        self.list.append(value)
+    
+    def printstack(self):
+        print(self.list)
+
+
+a = stack()
+
+str01 = "yesterday"
+revstr01 = ""
+
+for char in str01:
+    a.push(char)
+    
+#a.printstack()
+
+while a.size():
+    revstr01 = revstr01 + a.pop()
+
+print(revstr01)
+
+str02 = "123456789"
+
+print(sorted(str02, reverse=True))
